882c788229983b9d157d7187751b89e06db9a652
Simon Hyll <hyllsimon@gmail.com>
2024-02-20 18:46:22 +0100

    feat: here you go :)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,10 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use autopilot::mouse::Button;
 use std::sync::Mutex;
 use tauri::{AppHandle, Manager};
 use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers};
 extern crate autopilot;
-
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-#[tauri::command]
-fn click() {
-    autopilot::mouse::click(Button::Left, Some(0));
-}
 
 struct DelayState {
     delay: u64,
@@ -20,60 +14,69 @@ struct DelayState {
 async fn setup_hotkey(app: AppHandle) {
     // A shortcut using Alt + Shift + A to open the main window of the app
     let shortcut_delay =
-      tauri_plugin_global_shortcut::Shortcut::new(Some(Modifiers::SUPER), Code::F1);
+        tauri_plugin_global_shortcut::Shortcut::new(Some(Modifiers::SUPER), Code::KeyA);
     // The shortcut ID
     let shortcut_delay_id = shortcut_delay.id();
     // Only works on Desktop, register the plugin here
     #[cfg(desktop)]
-    app.app_handle().plugin(
-      tauri_plugin_global_shortcut::Builder::with_handler(move |app, key| {
-        loop {
-          let state = app.state::<Mutex<DelayState>>();
-          let mut enabled = state.lock().unwrap().enabled;
-          let delay = state.lock().unwrap().delay;
-          if key.id() == shortcut_delay_id {
-            enabled = !enabled;
-          }
-          while enabled {
-            if key.id() == shortcut_delay_id {
-              enabled = !enabled;
-            } else {
-              println!("click");
-              tauri::async_runtime::block_on(tokio::time::sleep(tokio::time::Duration::from_millis(delay)));
-            }
-          }
-        }
-      })
-      .build(),
-    ).unwrap();
-    app.app_handle().global_shortcut().register(shortcut_delay).unwrap();
+    app.app_handle()
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::with_handler(move |app, key| {
+                println!("key");
+                let state = app.state::<Mutex<DelayState>>();
+                let mut lock = state.lock().unwrap();
+                if key.id() == shortcut_delay_id {
+                    lock.enabled = !lock.enabled;
+                }
+                if !lock.enabled {
+                    return;
+                }
+                let handle = app.app_handle().clone();
+                tauri::async_runtime::spawn(async move {
+                    let mut enabled: bool = true;
+                    let mut delay: u64 = 1;
+                    while enabled {
+                        tokio::time::sleep(tokio::time::Duration::from_millis(delay)).await;
+                        let state = handle.state::<Mutex<DelayState>>();
+                        let lock = state.lock().unwrap();
+                        enabled = lock.enabled;
+                        delay = lock.delay;
+                        autopilot::mouse::click(Button::Left, Some(0));
+                        println!("click");
+                    }
+                    println!("exiting");
+                });
+            })
+            .build(),
+        )
+        .unwrap();
+    app.app_handle()
+        .global_shortcut()
+        .register(shortcut_delay)
+        .unwrap();
 }
 
 #[tauri::command]
 async fn set_delay(app: AppHandle, delay: u64) {
-  let state = app.state::<Mutex<DelayState>>();
-  let mut lock = state.lock().unwrap();
-  lock.delay = delay;
+    println!("2");
+    let state = app.state::<Mutex<DelayState>>();
+    let mut lock = state.lock().unwrap();
+    lock.delay = delay;
 }
 
-
-
-
-
-
-fn main() {      
+fn main() {
     tauri::Builder::default()
-    .plugin(tauri_plugin_shell::init())
-    .manage(Mutex::new(DelayState {
-      delay: 0,
-      enabled: false,
-    }))
-    .invoke_handler(tauri::generate_handler![set_delay])
-    .setup(|app| {
-      tauri::async_runtime::block_on(setup_hotkey(app.handle().clone()));
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![set_delay, click])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+        .plugin(tauri_plugin_shell::init())
+        .manage(Mutex::new(DelayState {
+            delay: 0,
+            enabled: false,
+        }))
+        .invoke_handler(tauri::generate_handler![set_delay])
+        .setup(|app| {
+            tauri::async_runtime::block_on(setup_hotkey(app.handle().clone()));
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![set_delay])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { invoke } from "@tauri-apps/api/core";
 
 //const toggle = document.querySelector('.toggle') as HTMLDivElement;
-const input = document.getElementById('input') as HTMLInputElement;
+const input: HTMLInputElement = document.getElementById(
+  "input"
+) as HTMLInputElement;
 //const off = document.querySelector('.off') as HTMLDivElement;
 let delay = 1000;
+input.value = delay.toString();
 
-invoke('set_delay', { delay: delay})
+input?.addEventListener("input", (e) => {
+  delay = parseInt((e.target as HTMLInputElement).value);
+  if (isNaN(delay)) delay = 1000;
+  if (delay < 1) delay = 1;
+  invoke("set_delay", { delay: delay });
+  console.log(delay);
+});
 
-input?.addEventListener('input', (e) => {
-    delay = parseInt((e.target as HTMLInputElement).value);
-    if (isNaN(delay)) delay = 1000;
-    if (delay < 1) delay = 1;
-    console.log(delay)
+const output: HTMLElement = document.getElementById("counter") as HTMLElement;
+let counter = 0;
+document.addEventListener("click", () => {
+  console.log("CLICK RECEIVED");
+  counter += 1;
+  output.innerText = counter.toString();
 });
